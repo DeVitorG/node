48314f7685834dfc1c022553ec40e73599cf2f67
Vitor <dev.vitor.gabriel@gmail.com>
2023-03-14 07:08:15 -0300

    feat: alterações;

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+import { appRoutes } from './lib/routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 /**
  * Método HTTP: Get(geralmente para trazer informações), Post(insere informações), Put(atualiza informações), Patch(atualizar uma informação específica), Delete(apaga informações).
  */
 
 app.register(cors)
-
-app.get('/habits', async  () => {
-    const habits = await prisma.habit.findMany({
-
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
